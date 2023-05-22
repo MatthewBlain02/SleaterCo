cdfd123dbacd3294567e562eacac64591c228806
Matthew Blain <53818917+MatthewBlain02@users.noreply.github.com>
2023-05-22 02:32:11 +0100

    Fixed Responsive Issue and cleaned code

---
+++ script/main.js
@@ -0,0 +1,14 @@
+ //----------------Start index.html Script----------------
+ //Declaring Variables
+ let fig1 = document.getElementById('fig-1');
+ let fig2 = document.getElementById('fig-2');
+ var w = window.innerWidth
+        || document.documentElement.clientWidth
+        || document.body.clientWidth; // <-- Sets The Width Of Device
+ if(w <= 425){ // <-- If The Device Is Equal or Smaller Than 425 (Most Phones) Then Change The Services To Display As Block
+    fig1.classList.remove('d-flex');
+    fig1.classList.add('d-block');
+    fig2.classList.remove('d-flex');
+    fig2.classList.add('d-block');
+    }
+//----------------End index.html Script----------------
